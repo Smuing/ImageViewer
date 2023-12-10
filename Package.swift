29a2ea58dd8e41acd07bc4682134bce73c50b07e
Sungmin Na <smuing0924@gmail.com>
2023-12-11 00:32:53 +0900

    storyboard

--- Package.swift
+++ Package.swift
@@ -24,7 +24,8 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "ImageViewer",
-            dependencies: ["SDWebImage"]),
+            dependencies: ["SDWebImage"],
+            resources: [.copy("Storyboards")]),
         .testTarget(
             name: "ImageViewerTests",
             dependencies: ["ImageViewer"]),
